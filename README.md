951f3950c02e06cce22ca8bd53e8798e379c7408
phillip-helms <phelms@caltech.edu>
2017-06-29 13:56:04 -0700

    Updated Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dmrg
+
+A simple, but inefficient, implementation of the Density Matrix Renormalization Group (DMRG) algorithm using Matrix Product States (MPS) and Matrix Product Operators (MPOs). 
+At this point, I have begun to implement a version of the code to solve the Heisenberg Model at any number of sites.
--- heisPractice/heisModel.py
+++ heisPractice/heisModel.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class heis_mpo:
     def __init__(self,nsite):
-        self.L = nsite
+        self.nsite = nsite
         self.d = 2 # Dimension of local state space
         self.h = 1 # First interaction parameter
         self.J = 1 # Second interaction parameter
@@ -13,15 +13,15 @@ class heis_mpo:
                                [[1,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[-1,0,0,0,0],[0,0,0,self.J/2,1]]]])
         # Construct M matrices (General versions of A and B matrices)
         self.M = []
-        for cnt in range(self.L):
+        for cnt in range(self.nsite):
             if cnt < nsite/2:
                 self.M.append(np.zeros([2,self.d**cnt,self.d**(cnt+1)]))
             else:
-                self.M.append(np.zeros([2,self.d**(self.L-cnt),self.d**(self.L-cnt-1)]))
+                self.M.append(np.zeros([2,self.d**(self.nsite-cnt),self.d**(self.nsite-cnt-1)]))
             self.M[cnt][:,0,0] = 1
         # Construct L and R arrays, or left and right blocks in DMRG parlance
-        self.R = [None]*(self.L+1)
-        self.F = [None]*(self.L+1)
+        self.R = [None]*(self.nsite+1)
+        self.L = [None]*(self.nsite+1)
 
     def W(self,site,sigma,sigmap):
         if sigmap is -1:
@@ -37,10 +37,11 @@ class heis_mpo:
 
     def initiate_r(self):
         self.R[-1] = np.array([1])
-        for site in range(self.L)[::-1]:
-            if site == self.L-1:
+        for site in range(self.nsite)[::-1]:
+            print(site)
+            if site == self.nsite-1:
                 self.R[site] = np.einsum('ijo,ink,k,jpm->nop',self.Wint[:,:,:,0],self.M[site],self.R[site+1],self.M[site])
-            if site == 0:
+            elif site == 0:
                 self.R[site] = np.array([1])
             else:
                 self.R[site] = np.einsum('ijlo,ink,klm,jpm->nop',self.Wint,self.M[site],self.R[site+1],self.M[site])
@@ -52,15 +53,17 @@ class heis_mpo:
         converged = False
         while not converged:
             # Sweep Right
-            for l in range(self.L-1):
-                # Solve eigenproblem for M(l)
-                np.einsum('',)
+            for l in range(self.nsite-1):
+                # Solve eigenproblem for M(l) (This should be replaced by iterative solver)
+                H = np.einsum('ijk,lmjn,opn->liomkp',self.L[site],self.Wint[site],self.R[site])
+                w,v = np.linalg.eig(H)
                 # Left-normalize M into A by SVD
                 # Multiply remaining SVD Matrices into M(l+1)
                 # Build the L expression by adding one more site
             # Sweep Left
-            for l in range(1,L)[::-1]:
+            for l in range(1,self.nsite)[::-1]:
                 # Solve eigenproblem for M(l)
+                H = np.einsum('ijk,lmjn,opn->liomkp',self.L[site],self.Wint[site],self.R[site])
                 # Right-normalize M(l) into B by SVD
                 # Multiply the remaining SVD Matrices into M(l-1)
                 # Build the R expression by adding one more site
