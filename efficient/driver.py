00afdfbc3abc7c1ad04a3dadb045fbd6fce39f74
philliphelms <phelms@caltech.edu>
2018-01-19 14:40:51 -0800

    Changed Driver slightly

--- efficient/driver.py
+++ efficient/driver.py
@@ -735,7 +735,7 @@ if vary_maxBondDim_2d_comp:
 
 if vary_maxBondDim_2d_sep_comp:
     N = 12
-    bondDimVec = [2,4,6]#,8,10,12,14,16,18,20,30,40,50,60,70,80,90,100,110,120,130,140,150]
+    bondDimVec = [2,4,6,8,10,12,14,16,18,20,30,40,50,60,70,80,90,100,110,120,130,140,150]
     # Run 1D Calculation for comparison
     x = mps_opt.MPS_OPT(N=N,
                         maxBondDim = bondDimVec,
