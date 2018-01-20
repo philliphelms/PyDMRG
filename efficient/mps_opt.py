switched to correct hamiltonian contraction for proper scaling

efficient/mps_opt.py
@@ -156,31 +156,18 @@ class MPS_OPT:
     def pyscf_optimization(self,i,direction):
         if self.verbose > 5:
             print('\t'*4+'Using Pyscf optimization routine')
-        bad_scaling = True
-        if bad_scaling:
-            H = self.einsum('jlp,lmin,kmq->ijknpq',self.F[i],self.mpo.W[i],self.F[i+1])
-            (n1,n2,n3,n4,n5,n6) = H.shape
-            H = np.reshape(H,(n1*n2*n3,n4*n5*n6))
-        if (self.hamType is "tasep") or (self.hamType is "sep") or (self.hamType is "sep_2d"):
-            if bad_scaling:
-                H = -H
+        (n1,n2,n3) = self.M[i].shape
         def opt_fun(x):
-            # function([x]) => [array_like_x]
             if self.verbose > 6:
                 print('\t'*5+'Eigenvalue Iteration')
-            # Might need a direction here to specify the reshaping of x
-            if not bad_scaling:
-                if direction is 'right':
-                    (n1,n2,n3) = self.M[i].shape
-                if direction is 'left':
-                    M_reshape = np.swapaxes(self.M[i],0,1)
-                    (n1,n2,n3) = M_reshape.shape
-                x = np.reshape(x,(n1,n2,n3))
-                in_sum1 =  self.einsum('',self.F[i+1],x)
-                #in_sum2 = self.einsum('',self.mpo.W[i],tmp1)
-                #return = self.einsum('',self.F[i],tmp2)
+            x_reshape = np.reshape(x,(n1,n2,n3))
+            in_sum1 =  self.einsum('ijk,lmk->ijlm',self.F[i+1],x_reshape)
+            in_sum2 = self.einsum('njol,ijlm->noim',self.mpo.W[i],in_sum1)
+            if (self.hamType is "tasep") or (self.hamType is "sep") or (self.hamType is "sep_2d"):
+                fin_sum = -self.einsum('pnm,noim->opi',self.F[i],in_sum2)
             else:
-                return self.einsum('ij,j->i',H,x)
+                fin_sum = self.einsum('pnm,noim->opi',self.F[i],in_sum2)
+            return np.reshape(fin_sum,-1)
         def precond(dx,e,x0):
             # function(dx, e, x0) => array_like_dx
             return dx
@@ -367,9 +354,6 @@ class MPS_OPT:
                 self.update_f(i,'right')
                 self.plot_observables()
                 self.plot_convergence(i)
-                print('F_shape = {}'.format(self.F[i].shape))
-                print('M_shape = {}'.format(self.M[i].shape))
-                print('W_shape = {}'.format(self.mpo.W[i].shape))
             # Left Sweep ---------------------------
             if self.verbose > 1:
                 print('\t'*0+'Left Sweep {}'.format(totIterCnt))
@@ -380,9 +364,6 @@ class MPS_OPT:
                 self.update_f(i,'left')
                 self.plot_observables()
                 self.plot_convergence(i)
-                print('F_shape = {}'.format(self.F[i].shape))
-                print('M_shape = {}'.format(self.M[i].shape))
-                print('W_shape = {}'.format(self.mpo.W[i].shape))
             # Check Convergence --------------------
             if np.abs(self.E-E_prev) < self.tol:
                 if self.maxBondDimInd is (len(self.maxBondDim)-1):