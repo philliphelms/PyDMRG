Working on correct normalization for left eigs

simple_scripts/normalization/n_site_model2.py
@@ -4,7 +4,7 @@ np.set_printoptions(precision=3,linewidth=1000)
 
 ######## Inputs ############################################################################
 # SEP Model
-N = 6
+N = 4
 alpha = 0.35  # In at left
 beta = 2/3    # Exit at right
 s = -1        # Exponential weighting
@@ -55,6 +55,8 @@ rwf_ed = rwf_ed[:,inds[-1]]
 # <L|R> = 1
 rwf_ed = rwf_ed/np.sum(rwf_ed)
 lwf_ed = lwf_ed/np.sum(lwf_ed*rwf_ed)
+print(rwf_ed)
+print(lwf_ed)
 ############################################################################################
 
 # Decompose States into MPS ################################################################
@@ -85,6 +87,9 @@ for i in range(N,1,-1):
     psil = np.reshape(psil,(2**(i-1),-1))
     (ur,sr,vr) = np.linalg.svd(psir,full_matrices=False)
     (ul,sl,vl) = np.linalg.svd(psil,full_matrices=False)
+    # make left eigenvector right-canonical
+    Xgauge = np.dot(vr,np.linalg.pinv(vl))
+    vl = np.dot(Xgauge,vl)
     Br = np.reshape(vr,(fbd_site[i-1],2,mbd_site[i]))
     Bl = np.reshape(vl,(fbd_site[i-1],2,mbd_site[i]))
     Br = Br[:mbd_site[i-1],:,:mbd_site[i]] 
@@ -94,18 +99,37 @@ for i in range(N,1,-1):
     Mr.insert(0,Br)
     Ml.insert(0,Bl)
     psir = np.einsum('ij,j->ij',ur[:,:mbd_site[i-1]],sr)
+    psil = np.einsum('ij,j,jk->ik',ul[:,:mbd_site[i-1]],sl,np.linalg.pinv(Xgauge))
     psil = np.einsum('ij,j->ij',ul[:,:mbd_site[i-1]],sl)
     # Check for Correct Canonicalization?
-    print('Check for Right normalization:\n{}'.format(np.einsum('ijk,ikl->jl',Mr[0],np.transpose(Mr[0],(0,2,1)))))
-    print('Check for Left  normalization:\n{}'.format(np.einsum('ijk,ikl->jl',Ml[0],np.transpose(Ml[0],(0,2,1)))))
     print('Check for Both  normalization:\n{}'.format(np.einsum('ijk,ikl->jl',Ml[0],np.transpose(Mr[0],(0,2,1)))))
-    print('Check for Both  normalization:\n{}'.format(np.einsum('ijk,ikl->jl',Mr[0],np.transpose(Ml[0],(0,2,1)))))
-    #Mr[0],Ml[0]
 Mr.insert(0,np.reshape(psir,(2,1,min(2,maxBondDim))))
 Ml.insert(0,np.reshape(psil,(2,1,min(2,maxBondDim))))
-#print(Mr)
-#print(Ml)
 ##############################################
+
+# Now Calculate State from MPS ###############
+occ = np.zeros((2**N,N),dtype=int)
+sum_occ = np.zeros(2**N)
+for i in range(2**N):
+    occ[i,:] = np.asarray(list(map(lambda x: int(x),'0'*(N-len(bin(i)[2:]))+bin(i)[2:])))
+    sum_occ[i] = np.sum(occ[i,:])
+# Calculate Wavefunction
+rwf_dmrg = np.zeros(2**N,dtype=np.complex128)
+lwf_dmrg = np.zeros(2**N,dtype=np.complex128)
+for i in range(2**N):
+    i_occ = occ[i,:]
+    tmp_matr = np.array([[1]])
+    tmp_matl = np.array([[1]])
+    for k in range(N):
+        tmp_matr = np.einsum('ij,jk->ik',tmp_matr,Mr[k][i_occ[k],:,:])
+        tmp_matl = np.einsum('ij,jk->ik',tmp_matl,Ml[k][i_occ[k],:,:])
+    #print(np.sum(tmp_mat-tmp_matl))
+    rwf_dmrg[i] = tmp_matr[0,0]
+    lwf_dmrg[i] = tmp_matl[0,0]
+print(rwf_dmrg)
+print(lwf_dmrg)
+##############################################
+
 """
 # Create F ###################################
 F = []