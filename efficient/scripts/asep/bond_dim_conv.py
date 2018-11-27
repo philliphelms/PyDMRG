ee87d113a0670f7f6d41b528e8e26e7eb2fa581a
philliphelms <phelms@caltech.edu>
2018-11-27 15:34:22 -0800

    change bond dim conv script

--- efficient/scripts/asep/bond_dim_conv.py
+++ efficient/scripts/asep/bond_dim_conv.py
@@ -14,41 +14,49 @@ plt.rcParams['text.latex.preamble'] = [r'\boldmath']
 plt.rc('font', family='serif')
 plt.rcParams['text.latex.unicode']=False
 np.set_printoptions(suppress=True)
-np.set_printoptions(precision=10)
+np.set_printoptions(precision=16)
+np.set_printoptions(linewidth=1000)
 plt.style.use('ggplot') #'fivethirtyeight') #'ggplot'
 
-N = int(argv[1])
+N = 10
 rho_r = 0.5
 rho_l = 0.5
-s = np.linspace(-2,-0.5,15)
-p = np.array([0.2])
-bd = [2,4,6,8,10,20,30,40,50,60,70,80,90,100,125,150,175,200,250,300,350,400,450,500,600,700,800,900,1000]
-maxIter = [2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2]
+sVec = np.linspace(-2,-0.5,15)
+pVec = np.array([0.1])
+bdVec = [2,4,6,8,10]#,20,30,40,50]#,60,70,80,90,100,125,150,175,200,250,300,350,400,450,500,600,700,800,900,1000]
+maxIterVec = [10,10,10,10,10]#,10,10,10,10]#[2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2]
+
+
 print('s =')
-for i in range(len(s)):
-    print(s[i])
+for i in range(len(sVec)):
+    print(sVec[i])
 print('p =')
-for i in range(len(p)):
-    print(p[i])
+for i in range(len(pVec)):
+    print(pVec[i])
 print('\n\n')
-fname = str(int(time.time()))+'_'+'asep_bond_dim_check_N'+str(N)+'_data_p'+str(len(p))+'s'+str(len(s))+'bd'+str(len(bd))
-CGF = np.zeros((len(p),len(s),len(bd)),dtype=np.complex128)   # CGF
-nPart = np.zeros((len(p),len(s),len(bd)),dtype=np.complex128) # Number of particles
-EE = np.zeros((len(p),len(s),len(bd)),dtype=np.complex128)    # Entanglement Entropy
-density = np.zeros((len(p),len(s),len(bd),N),dtype=np.complex128)
-current = np.zeros((len(p),len(s),len(bd),N),dtype=np.complex128)
-for i in range(len(p)):
-    for j in range(len(s)):
-        print('s = {}'.format(s[j]))
-        print('p = {}'.format(p[i]))
+
+
+fname = str(int(time.time()))+'_'+'asep_bond_dim_check_N'+str(N)+'_data_p'+str(len(pVec))+'s'+str(len(sVec))+'bdVec'+str(len(bdVec))
+CGF = np.zeros((len(pVec),len(sVec),len(bdVec)),dtype=np.complex128)   # CGF
+EE = np.zeros((len(pVec),len(sVec),len(bdVec)),dtype=np.complex128)    # Entanglement Entropy
+EEs = np.zeros((len(pVec),len(sVec),len(bdVec),max(bdVec)),dtype=np.complex128)   # Entanglement Spectrum
+
+
+for pind,p in enumerate(pVec):
+    for sind,s in enumerate(sVec):
+        print('s = {}'.format(s))
+        print('p = {}'.format(p))
         x = mps_opt.MPS_OPT(N=N,
-                            maxBondDim = bd,
-                            maxIter = maxIter,
+                            maxBondDim = bdVec,
+                            maxIter = maxIterVec,
                             add_noise=False,
                             hamType = "sep",
-                            verbose = 3,
-                            hamParams = (rho_l,1-rho_l,p[i],1-p[i],1-rho_r,rho_r,s[j]))
+                            verbose = 4,
+                            hamParams = (rho_l,1-rho_l,p,1-p,1-rho_r,rho_r,s))
         x.kernel()
-        CGF[i,j,:] = x.bondDimEnergies
-        EE[i,j,:] = x.bondDimEntanglement
-        np.savez(fname,s=s,p=p,CGF=CGF,EE=EE)
+        CGF[pind,sind,:] = x.bondDimEnergies
+        EE[pind,sind,:] = x.bondDimEntanglement
+        EEs[pind,sind,:,:] = x.bondDimEntanglementSpec
+        for i in range(len(bdVec)):
+            print('EEs[{}]={}'.format(bdVec[i],EEs[pind,sind,i,:]))
+        np.savez(fname,s=sVec,p=pVec,CGF=CGF,EE=EE)
